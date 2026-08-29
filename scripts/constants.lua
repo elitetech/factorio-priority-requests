77d2af678b3fa5c0a063fb7bbea9baa43e448adc
Koty Bashford <koty.bashford@gmail.com>
2026-08-29 02:19:06 -0400

    Fix capitalization in frame title and adjust panel width to 178

--- scripts/constants.lua
+++ scripts/constants.lua
@@ -4,7 +4,7 @@ local constants = {
   PRIORITY_TAG_KEY = "fpr_priority",
   STATUS_VALUE = "fpr_status_value",
   REQUESTS_LIST = "fpr_requests_list",
-  PANEL_WIDTH = 175,
+  PANEL_WIDTH = 178,
   DROPDOWN_WIDTH = 60,
   PRIORITY_LEVELS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 }
